8d0cff1b1d8d9c796b75b6da3f39d53939d33ca1
frode <frode@frodes-mbp.lan>
2019-08-26 21:01:42 +0200

    some changes

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 #specify a base image
 from node:alpine
-
 workdir /usr/app
 #install some dependencies
 copy ./package.json ./
 
 run npm install
 copy ./ ./
-#default command
+#default command  frode
 cmd ["npm", "start"]
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send('kjempe gla i deg Maria Hilsen Pappa');
+  res.send('kjempe gla gla i deg Maria Hilsen Pappa din frode');
 });
  
 app.listen(8080, () => {
