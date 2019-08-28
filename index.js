a9a3558b36df53e2b613ed73f43c15446b618be0
frode23 <frode.gundersen23@gmail.com>
2019-08-28 17:57:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send('pappa er kjempe gla gla i deg Maria Hilsen Pappa din frode');
+  res.send('pappa frode er kjempe gla gla i deg Maria Hilsen Pappa din frode');
 });
  
 app.listen(8080, () => {
