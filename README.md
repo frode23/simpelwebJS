45692ec9287968542662192f73d95d5ac1eea8f3
frode23 <frode.gundersen23@gmail.com>
2019-08-29 22:18:50 +0200

    ad a linje and readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # simpelweb
+#dte er bra
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
  
 app.get('/', (req, res) => {
   res.send('edit on master watch u2b 29.08 19.19 pappa frode er kjempe gla gla i deg Maria Hilsen Pappa din frode');
+  res.send('edit on master watch u2b 29.08 19.19 pappa frode er kjempe gla gla i deg Maria Hilsen Pappa din frode');
 });
  
 app.listen(8080, () => {
